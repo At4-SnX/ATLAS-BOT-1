8279307357d99b1b5a1c228f2045d22a7d403d90
Adam Raynaut <snx123123@gmail.com>
2026-07-24 07:07:47 +0200

    sdqz

--- config/messages.js
+++ config/messages.js
@@ -9,9 +9,9 @@
 module.exports = {
   SERVER_NAME: "ATLAS RP",
 
-  ARRIVAL_MESSAGE: "Salut à toi {user} Bienvenue sur ATLAS RP !",
+  ARRIVAL_MESSAGE: "**🌍 | Salut à toi {user}, Bienvenue sur ATLAS RP !**",
 
-  DEPARTURE_MESSAGE: "Aurevoir et à bientôt, ce fut un plaisir de t'accueillir sur ATLAS RP...",
+  DEPARTURE_MESSAGE: "**🌍 | Aurevoir et à bientôt, ce fut un plaisir de t'accueillir sur ATLAS RP...**",
 
   // Couleur des embeds (hexadécimal)
   EMBED_COLOR: 0x2c2568,
